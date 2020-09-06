0e18810c4422842192b3d6e22251576fba7fb067
Ritik Tyagi <cu.17bcs1090@gmail.com>
2020-09-06 12:08:10 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -143,7 +143,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,"../",'client','build')))
   
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../",'build','client','index.html'))
+        res.sendFile(path.join(__dirname,"../",'client','build','index.html'))
     })
   }
 
